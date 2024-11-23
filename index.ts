85a4d4669a78fdd32eb3105a7e5c273100131fc8
yqiaoxu <yqiaoxu@tencent.com>
2024-11-23 20:14:13 +0800

    feat: 增加日志

--- index.ts
+++ index.ts
@@ -3,6 +3,11 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const app = express();
 
+const router = express.Router()
+router.get('/health', (req, res) => {
+    res.send('server is health!')
+})
+
 app.use(
     '/v1/',
     createProxyMiddleware({
@@ -10,9 +15,11 @@ app.use(
       changeOrigin: true,
       on: {
         proxyReq: (proxyReq, req, res) => {
-            proxyReq.setHeader('Authorization', `Bearer ${process.env.API_KEY}`);
+          console.log('proxyReq: ', req);
+          proxyReq.setHeader('Authorization', `Bearer ${process.env.API_KEY}`);
         },
         proxyRes: (proxyRes, req, res) => {
+          console.log('proxyRes: ', res);
             proxyRes.headers['Access-Control-Allow-Origin'] = '*';
             proxyRes.headers['Access-Control-Allow-Headers'] =
               'Content-Type,Content-Length, Authorization, Accept,X-Requested-With';
